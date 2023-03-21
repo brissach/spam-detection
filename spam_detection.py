19e6265f3850ef8bf864c20cfa2688241cee8cb8
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-21 15:19:40 +0100

    add `decay`, check for matches

--- spam_detection.py
+++ spam_detection.py
@@ -4,31 +4,38 @@ last_line = None
 frequency = 0
 frequency_scaler = 0.1
 frequency_multiplier = 2
+frequency_decay = 0.08
 
 def process(line):
     line = line.strip().lower()
-    line = re.sub(r"[^a-z0-9 ]", " ", line)
-    global last_line, frequency, frequency_scaler, frequency_multiplier
+    line = re.sub(r"[^a-z0-9 ]", "", line)
+    global last_line, frequency, frequency_scaler, frequency_multiplier, frequency_decay
     if last_line is None:
         last_line = line
         return
-    sim = calculate_diff(line)
+    sim = calculate_sim(line)
+    match = []
+    for i in range(min(len(last_line), len(line))):
+        if last_line[i] == line[i]:
+            match.append(last_line[i])
+        else: 
+            break
+    match = "".join(match)
+    if len(match) > 0:
+        sim *= 1.0 + (len(match) / max(len(last_line), len(line)))
     if sim > 0.25:
         frequency += frequency_scaler * frequency_multiplier ** frequency
         frequency = min(frequency, 1)
-        if frequency >= 1:
+        if frequency >= 0.5:
             print("Spam detected: %s" % line)
-            print(frequency + sim)
         else:
             print("Possible spam detected: %s" % last_line)
-        print(frequency)
     else:
-        frequency -= frequency_scaler * frequency_multiplier ** frequency
+        frequency -= max(frequency_decay, frequency_scaler * frequency)
     last_line = line
 
-def calculate_diff(line):
-    if last_line is None:
-        return 0.0
+def calculate_sim(line):
+    if last_line is None: return 0.0
     similarity = 0.0
     for i in range(min(len(last_line), len(line))):
         if last_line[i] == line[i]:
@@ -40,7 +47,12 @@ tests = [
     "Spam Xz0<;W",
     "Spam 8-'`-",
     "Hello World!",
-
+    "Spam ^2s¤#c",
+    "Spam Xz0<;W",
+    "Spam 8-'`-",
+    "Spam ^2s¤#c",
+    "Spam Xz0<;W",
+    "Spam 8-'`-",
 ]
 
 for test in tests:
